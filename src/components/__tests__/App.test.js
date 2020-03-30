63e5709c9d5006133d665be978126706cd9ef68e
darrylgit <darrylsmithup@gmail.com>
2020-03-30 15:24:15 -0400

    Use Enzyme to test rendering

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,27 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "../App";
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
+  const wrapped = shallow(<App />);
 
-  ReactDOM.render(<App />, div);
-
-  console.log(div.innerHTML);
-
-  expect(div.innerHTML).toContain("Comment Box");
-
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
-it("shows a comment box", () => {
-  const div = document.createElement("div");
+it("shows a comment list", () => {
+  const wrapped = shallow(<App />);
 
-  ReactDOM.render(<App />, div);
-
-  console.log(div.innerHTML);
-
-  expect(div.innerHTML).toContain("Comment List");
-
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
