aa0c437d6307b753a1ecf7e50d3737d959994819
darrylgit <darrylsmithup@gmail.com>
2020-03-30 15:38:36 -0400

    Wire up input to state

--- src/components/CommentBox.jsx
+++ src/components/CommentBox.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
 class CommentBox extends React.Component {
+  state = { comment: "" };
+
+  handleChange = e => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // Call an action creator
+
+    this.setState({ comment: "" });
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a Comment</h4>
-        <textarea />
+        <textarea value={this.state.comment} onChange={this.handleChange} />
         <div>
           <button>Submit Comment</button>
         </div>
