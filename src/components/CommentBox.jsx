a29a76f68e2faba86aa19621fbad0022eabb2860
darrylgit <darrylsmithup@gmail.com>
2020-03-30 15:34:23 -0400

    Flesh out CommentBox

--- src/components/CommentBox.jsx
+++ src/components/CommentBox.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends React.Component {
+  render() {
+    return (
+      <form>
+        <h4>Add a Comment</h4>
+        <textarea />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;
