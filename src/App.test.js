2e1acbcaf917d4b91be3bbe8215521ebe0ed3686
darrylgit <darrylsmithup@gmail.com>
2020-03-30 11:20:40 -0400

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders without crashing", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const hello = getByText(/Hi there!/);
+  expect(hello).toBeInTheDocument();
 });
