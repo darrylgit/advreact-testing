ad408eae90c8f52cd901ead465aa0ba30171517f
darrylgit <darrylsmithup@gmail.com>
2020-03-30 11:31:06 -0400

    Create new project structure

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders without crashing", () => {
-  const { getByText } = render(<App />);
-  const hello = getByText(/Hi there!/);
-  expect(hello).toBeInTheDocument();
-});
