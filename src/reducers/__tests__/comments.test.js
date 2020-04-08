2407c05f43fc65732b1879813a77d7f41bb4e3f1
darrylgit <darrylsmithup@gmail.com>
2020-04-08 16:05:23 -0400

    Write first test for comments reducer

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -1,1 +1,12 @@
 import commentsReducer from "reducers/comments";
+import { SAVE_COMMENT } from "actions/types";
+
+it("handles SAVE_COMMENT", () => {
+  const action = {
+    type: SAVE_COMMENT,
+    payload: "New Comment"
+  };
+  const newState = commentsReducer([], action);
+
+  expect(newState).toEqual(["New Comment"]);
+});
