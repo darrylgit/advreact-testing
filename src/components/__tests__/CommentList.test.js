008c2c381355d28dc4e6318f7f14eeaf62fcb514
darrylgit <darrylsmithup@gmail.com>
2020-04-08 18:05:56 -0400

    Use render() to test text contents of CommentList

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -20,3 +20,8 @@ beforeEach(() => {
 it("creates one li per comment", () => {
   expect(wrapped.find("li").length).toEqual(2);
 });
+
+it("shows the text for each comment", () => {
+  expect(wrapped.render().text()).toContain("Comment 1");
+  expect(wrapped.render().text()).toContain("Comment 2");
+});
