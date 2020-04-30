6cc99209b0da03f5a3b0cb3f3ea1f8d5ec2ac9a8
darrylgit <darrylsmithup@gmail.com>
2020-04-29 20:10:34 -0400

    Flesh out rudimentary integration test

--- src/components/CommentBox.jsx
+++ src/components/CommentBox.jsx
@@ -27,7 +27,9 @@ class CommentBox extends React.Component {
             <button>Submit Comment</button>
           </div>
         </form>
-        <button onClick={this.props.fetchComments}>Fetch Comments</button>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>
+          Fetch Comments
+        </button>
       </div>
     );
   }
